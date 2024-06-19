bb6877ef021fe98d021fd42334fb7c7d1245a9f2
Javier Ramirez <javierbaquero513@gmail.com>
2024-06-19 16:17:31 -0500

    actualizacion front

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -147,7 +147,7 @@ const Root = () => {
               </header>
               <main className="flex-grow container mx-auto px-4 py-8">
                 <section className="my-8">
-                  <h2 className="text-3xl font-bold mb-4">Quiénes Somos</h2>
+                  <h2 className="text-3xl font-bold mb-4">¿Quiénes Somos?</h2>
                   <p className="text-lg leading-relaxed text-gray-700">
                     Bienvenido a Inventarify, el revolucionario sistema de
                     gestión de inventarios y control de ventas diseñado para
